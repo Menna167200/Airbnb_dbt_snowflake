44cfd6516c6212fffcda5e227c039264393c9636
Menna167200 <menna.mnon2003@gmail.com>
2026-06-14 03:37:24 +0300

    Add orchesteration to the pipeline

--- tests/generic/booking_test.sql
+++
@@ -1,7 +0,0 @@
-{% test booking_test(model, column_name) %}
-
-    SELECT {{column_name}} 
-    FROM {{model}}
-    WHERE {{column_name}} NOT IN ('confirmed', 'cancelled', 'unknown') --RETURNS FALSE IF ANY OTHER VALUE IS PRESENT IN THE COLUMN
-
-{% endtest %}
